54c40e5b7aff830674da89e18c1aab074a993507
Ernie <ernestjohnson80@yahoo.com>
2020-12-12 14:01:09 -0800

    deccommit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ernie <code>was here</code> save and load.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"></a>
       </header>
     </div>
   );
